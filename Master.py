fbb0b6bd782d5c87d239f6b65e71d73c9696d084
Zhiyuan Chen <zc@int.ac.cn>
2019-03-05 16:21:31 +1100

    完善方法

--- Master.py
+++ Master.py
@@ -8,11 +8,11 @@ class Game(object):
         self.game_status = 0
         self.game_type = 0
 
-    def get_game_status(self, game_id):
-        return game_id.game_status
+    def get_game_status(self):
+        return self.game_status
 
-    def set_game_status(self, game_id, game_status):
-        game_id.game_status = game_status
+    def set_game_status(self, game_status):
+        self.game_status = game_status
 
 
 class Character(Enum):
@@ -29,9 +29,12 @@ class Character(Enum):
     wy = '乌鸦'
     Master = '神'
 
+    def heal(self, player_healed):
+        self.set_player_healed(True)
+
 
 class Player(object):
-    def __init(self, player_id):
+    def __init__(self, player_id):
         self.player_id = player_id
         self.player_status = 0
         self.player_character = 0
@@ -39,3 +42,32 @@ class Player(object):
         self.player_healed = False
         self.player_empty = 0
 
+    def get_player_status(self):
+        return self.player_status
+
+    def set_player_status(self, player_status):
+        self.player_status = player_status
+
+    def get_player_character(self):
+        return self.player_character
+
+    def set_player_character(self, player_character):
+        self.player_character = player_character
+
+    def get_player_hugged(self):
+        return self.player_hugged
+
+    def set_player_hugged(self, player_hugged):
+        self.player_hugged = player_hugged
+
+    def get_player_healed(self):
+        return self.player_healed
+
+    def set_player_healed(self, player_healed):
+        self.player_healed = player_healed
+
+    def get_player_empty(self):
+        return self.player_empty
+
+    def set_player_empty(self, player_empty):
+        self.player_empty = player_empty
