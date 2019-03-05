9881c009283c92400ec9c625bd4704b52a8ddcaf
Zhiyuan Chen <zc@int.ac.cn>
2019-03-05 16:12:44 +1100

    添加角色枚举类

--- Master.py
+++ Master.py
@@ -15,6 +15,21 @@ class Game(object):
         game_id.game_status = game_status
 
 
+class Character(Enum):
+    Nill = '无'
+    hhd = '花蝴蝶'
+    jc = '警察'
+    jjs = '狙击手'
+    ys = '医生'
+    ss = '杀手'
+    mfs = '魔法师'
+    sl = '森林老人'
+    sm = '善民'
+    em = '饿民'
+    wy = '乌鸦'
+    Master = '神'
+
+
 class Player(object):
     def __init(self, player_id):
         self.player_id = player_id
